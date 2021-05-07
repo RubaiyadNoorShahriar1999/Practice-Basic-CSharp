5c03c66cd8c6e0ffe1785900396b34f7b711176b
RubaiyadNoorShahriar1999 <77986516+RubaiyadNoorShahriar1999@users.noreply.github.com>
2021-05-08 04:56:07 +0600

    Inheritance Practice

---
+++ UniversityManagement/UniversityManagement/CommonClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityManagement
+{
+    class CommonClass
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string id;
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private string dob;
+
+        public string Dob
+        {
+            get { return dob; }
+            set { dob = value; }
+        }
+
+        private string bloodgroup;
+
+        public string BloodGroup
+        {
+            get { return bloodgroup; }
+            set { bloodgroup = value; }
+        }
+
+        public void showInfo(string name , string id , string dob , string bloodGroup)
+        {
+            Console.WriteLine("Name : {0} ", name);
+            Console.WriteLine("ID : {0 }" , id);
+            Console.WriteLine("Date of Birth : {0} " , dob);
+            Console.WriteLine("Blood Group : {0} " , bloodgroup);
+        }
+        
+        
+        
+    }
+}
---
+++ UniversityManagement/UniversityManagement/OSA.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityManagement
+{
+    class OSA : CommonClass
+    {
+        public OSA()
+        {
+            Console.WriteLine("Enter your room number : ");
+            room = Convert.ToInt32(Console.ReadLine());
+        }
+        private int room;
+
+        public int Room
+        {
+            get { return room; }
+            set { room = value; }
+        }
+
+        new public void showInfo(string name, string id, string dob, string bloodgroup) //Function overriding
+        {
+            this.Name = name;
+            this.Id = id;
+            this.Dob = dob;
+            this.BloodGroup = bloodgroup;
+
+            base.showInfo(Name ,Id ,Dob ,BloodGroup);
+            Console.WriteLine("Room Number : {0} " , room);
+        }
+        
+    }
+}
